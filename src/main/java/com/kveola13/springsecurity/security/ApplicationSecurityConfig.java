e95f4f780afa76854f0a545f28d809e5c7717d00
olakv <kveola13@student.westerdals.no>
2020-06-06 11:53:51 +0200

    Fixes to the security

--- src/main/java/com/kveola13/springsecurity/security/ApplicationSecurityConfig.java
+++ src/main/java/com/kveola13/springsecurity/security/ApplicationSecurityConfig.java
@@ -1,13 +1,28 @@
 package com.kveola13.springsecurity.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
@@ -17,5 +32,24 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authenticated()
                 .and()
                 .httpBasic();
+    }
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        UserDetails adminUser = User
+                .builder()
+                .username("Geralt")
+                .password(passwordEncoder.encode("Rivia"))
+                .roles("Wolf")
+                .build();
+
+        UserDetails newbUser = User.builder()
+                .username("Ciri")
+                .password(passwordEncoder.encode("Portal"))
+                .roles("Newb")
+                .build();
+
+        return new InMemoryUserDetailsManager(adminUser, newbUser);
     }
 }
