72deecc3e9fc883e57b1686dd8f366ba1396a15e
olakv <kveola13@student.westerdals.no>
2020-06-07 13:32:23 +0200

    Added another role to config

--- src/main/java/com/kveola13/springsecurity/security/ApplicationUserRole.java
+++ src/main/java/com/kveola13/springsecurity/security/ApplicationUserRole.java
@@ -8,7 +8,8 @@ import static com.kveola13.springsecurity.security.ApplicationUserPermission.*;
 
 public enum ApplicationUserRole {
     Newb(Sets.newHashSet()),
-    Wolf(Sets.newHashSet(STATS_READ, STATS_WRITE, UNIT_READ, UNIT_WRITE));
+    Wolf(Sets.newHashSet(STATS_READ, STATS_WRITE, UNIT_READ, UNIT_WRITE)),
+    Bard(Sets.newHashSet());
 
     private final Set<ApplicationUserPermission> permissions;
 
