be8e1e55b13c354f7d1c81a023b60fb10533228d
olakv <kveola13@student.westerdals.no>
2020-06-08 23:11:19 +0200

    fixed management controller

--- src/main/java/com/kveola13/springsecurity/unit/UnitManagementController.java
+++ src/main/java/com/kveola13/springsecurity/unit/UnitManagementController.java
@@ -1,5 +1,6 @@
 package com.kveola13.springsecurity.unit;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
@@ -14,24 +15,28 @@ public class UnitManagementController {
             new Unit(3, "Assassin"));
 
     @GetMapping
+    @PreAuthorize("hasAnyRole('ROLE_WOLF, ROLE_BARD')")
     public List<Unit> getAllUnits() {
         System.out.println("Getting all units");
         return UNITS;
     }
 
     @PostMapping
+    @PreAuthorize("hasAuthority('unit:write')")
     public void recruitNewUnit(@RequestBody Unit unit) {
         System.out.println("Recruited unit:");
         System.out.println(unit);
     }
 
     @DeleteMapping(path = "{unitId}")
+    @PreAuthorize("hasAuthority('unit:write')")
     public void deleteUnit(@PathVariable("unitId") Integer unitId) {
         System.out.println("Sold unit:");
         System.out.println(unitId);
     }
 
     @PutMapping(path = "{unitId}")
+    @PreAuthorize("hasAuthority('unit:write')")
     public void updateUnit(@PathVariable("unitId") Integer unitId, @RequestBody Unit unit) {
         System.out.println("Updated unit:");
         System.out.println(String.format("%s, %s", unitId, unit));
