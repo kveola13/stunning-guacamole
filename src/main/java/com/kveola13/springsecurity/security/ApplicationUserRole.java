eec4d6df119c083c7013b81330f5603107b8baf1
olakv <kveola13@student.westerdals.no>
2020-06-08 16:06:18 +0200

    fixed role

--- src/main/java/com/kveola13/springsecurity/security/ApplicationUserRole.java
+++ src/main/java/com/kveola13/springsecurity/security/ApplicationUserRole.java
@@ -1,8 +1,10 @@
 package com.kveola13.springsecurity.security;
 
 import com.google.common.collect.Sets;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import static com.kveola13.springsecurity.security.ApplicationUserPermission.*;
 
@@ -18,6 +20,15 @@ public enum ApplicationUserRole {
     }
 
     public Set<ApplicationUserPermission> getPermissions() {
+        return permissions;
+    }
+
+    public Set<SimpleGrantedAuthority> grantedAuthorities() {
+        Set<SimpleGrantedAuthority> permissions = getPermissions()
+                .stream()
+                .map(applicationUserPermission -> new SimpleGrantedAuthority(applicationUserPermission.getPermission()))
+                .collect(Collectors.toSet());
+        permissions.add(new SimpleGrantedAuthority("ROLE_" + this.name()));
         return permissions;
     }
 }
