24826d54c72bb04615ffca0f879148e565bad0de
olakv <kveola13@student.westerdals.no>
2020-06-10 22:35:15 +0200

    testing without ant matchers

--- src/main/java/com/kveola13/springsecurity/security/ApplicationSecurityConfig.java
+++ src/main/java/com/kveola13/springsecurity/security/ApplicationSecurityConfig.java
@@ -31,7 +31,7 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
+        http
                 .authorizeRequests()
                 .antMatchers("/", "index", "/css/*", "/js/*")
                 .permitAll()
