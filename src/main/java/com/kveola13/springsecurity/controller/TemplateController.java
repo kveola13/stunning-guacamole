18f6b48d0e8a950a6ea5b9bcfcb6d42b89e4fbf3
olakv <kveola13@student.westerdals.no>
2020-06-12 15:09:32 +0200

    fix to template

--- src/main/java/com/kveola13/springsecurity/controller/TemplateController.java
+++ src/main/java/com/kveola13/springsecurity/controller/TemplateController.java
@@ -1,11 +1,13 @@
 package com.kveola13.springsecurity.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
 public class TemplateController {
+    @GetMapping("login")
     public String getLoginView(){
         return "login";
     }
