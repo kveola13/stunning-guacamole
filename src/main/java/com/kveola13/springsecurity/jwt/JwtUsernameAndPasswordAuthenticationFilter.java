5d2e4af41ee73f0f564cbaed2c5efd95b15d7db4
olakv <kveola13@student.westerdals.no>
2020-06-16 17:50:27 +0200

    added new request

--- src/main/java/com/kveola13/springsecurity/jwt/JwtUsernameAndPasswordAuthenticationFilter.java
+++ src/main/java/com/kveola13/springsecurity/jwt/JwtUsernameAndPasswordAuthenticationFilter.java
@@ -1,15 +1,23 @@
 package com.kveola13.springsecurity.jwt;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 public class JwtUsernameAndPasswordAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        try {
+            UsernameAndPasswordAuthenticationRequest usernameAndPasswordAuthenticationRequest =
+                    new ObjectMapper().readValue(request.getInputStream(), UsernameAndPasswordAuthenticationRequest.class);
+        } catch (IOException ioException) {
+            throw new RuntimeException(ioException);
+        }
         return super.attemptAuthentication(request, response);
     }
 }
