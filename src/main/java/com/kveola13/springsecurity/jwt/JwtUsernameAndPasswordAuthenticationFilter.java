a8cf84a3d0f1d131841331c65572871b62e94cda
olakv <kveola13@student.westerdals.no>
2020-06-16 17:47:49 +0200

    added new filter

---
+++ src/main/java/com/kveola13/springsecurity/jwt/JwtUsernameAndPasswordAuthenticationFilter.java
@@ -0,0 +1,15 @@
+package com.kveola13.springsecurity.jwt;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class JwtUsernameAndPasswordAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        return super.attemptAuthentication(request, response);
+    }
+}
