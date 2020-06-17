c5a53e9b0cc076fbab798805835120947f14dca3
olakv <kveola13@student.westerdals.no>
2020-06-17 19:15:01 +0200

    Fix to filter

--- src/main/java/com/kveola13/springsecurity/jwt/JwtUsernameAndPasswordAuthenticationFilter.java
+++ src/main/java/com/kveola13/springsecurity/jwt/JwtUsernameAndPasswordAuthenticationFilter.java
@@ -1,15 +1,21 @@
 package com.kveola13.springsecurity.jwt;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.time.LocalDate;
+import java.util.Date;
 
 public class JwtUsernameAndPasswordAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
@@ -28,10 +34,23 @@ public class JwtUsernameAndPasswordAuthenticationFilter extends UsernamePassword
                     usernameAndPasswordAuthenticationRequest.getUsername(),
                     usernameAndPasswordAuthenticationRequest.getPassword()
             );
-            authenticationManager.authenticate(authentication);
+            return authenticationManager.authenticate(authentication);
         } catch (IOException ioException) {
             throw new RuntimeException(ioException);
         }
-        return super.attemptAuthentication(request, response);
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request,
+                                            HttpServletResponse response,
+                                            FilterChain chain,
+                                            Authentication authResult) throws IOException, ServletException {
+        String token = Jwts.builder().setSubject(authResult.getName())
+                .claim("authorities", authResult.getAuthorities())
+                .setIssuedAt(new Date())
+                .setExpiration(java.sql.Date.valueOf((LocalDate.now().plusWeeks(2))))
+                .signWith(Keys.hmacShaKeyFor("secureandlongstringforyourentertainment".getBytes()))
+                .compact();
+        response.addHeader("Authorization", "Bearer " + token);
     }
 }
