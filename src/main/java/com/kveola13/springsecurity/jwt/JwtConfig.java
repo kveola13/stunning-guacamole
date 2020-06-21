a82c1b495fb5f2bb802334d49968290903abc817
olakv <kveola13@student.westerdals.no>
2020-06-21 12:37:52 +0200

    added config

---
+++ src/main/java/com/kveola13/springsecurity/jwt/JwtConfig.java
@@ -0,0 +1,7 @@
+package com.kveola13.springsecurity.jwt;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "application.jwt")
+public class JwtConfig {
+}
